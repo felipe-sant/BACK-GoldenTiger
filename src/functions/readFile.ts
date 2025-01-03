ab39256b1aa838642a8b40118d36112a7674dbb9
Luiz Felipe dos Santos <felipe.pinghang2@gmail.com>
2025-01-03 12:59:50 +0000

    feat: adding documentation page

---
+++ src/functions/readFile.ts
@@ -0,0 +1,9 @@
+import fs from 'fs';
+import path from "path";
+
+function readFile(filePatch: string): any {
+    const data = fs.readFileSync(path.resolve(__dirname, `../../public/${filePatch}`), 'utf8');
+    return data;
+}
+
+export default readFile
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import playRoutes from './routes/play.routes';
+import readFile from './functions/readFile';
 
 const app = express();
 const port = process.env.PORT || 3001
@@ -8,6 +9,10 @@ const port = process.env.PORT || 3001
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/docs', (_: Request, res: Response) => {
+    const indexPage = readFile("index.html")
+    res.status(200).send(indexPage)
+})
 app.use('/api', playRoutes)
 
 app.listen(port, () => {
