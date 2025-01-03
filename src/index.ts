d84a58e0d414dc33aa522fe1f9921d4dbde8c2ef
Luiz Felipe dos Santos <felipe.pinghang2@gmail.com>
2025-01-03 13:06:23 +0000

    feat: add redirect to documentation page in default directory

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,21 @@ const port = process.env.PORT || 3001
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (_: Request, res: Response) => {
+    res.redirect('/api/docs');
+});
+
+app.get('/api', (_: Request, res: Response) => {
+    res.redirect('/api/docs');
+});
+
 app.use('/api/docs', (_: Request, res: Response) => {
     const indexPage = readFile("index.html")
     res.status(200).send(indexPage)
 })
+
 app.use('/api', playRoutes)
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
