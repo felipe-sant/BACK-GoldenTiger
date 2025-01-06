5f7e3f4219ce8cae4bdc204fa8760d999cc7528e
Luiz Felipe <felipe.pinghang2@gmail.com>
2025-01-06 20:50:31 -0300

    fix: adding request limit on routes #1

--- src/routes/play.routes.ts
+++ src/routes/play.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import PlayController from '../controllers/play.controller';
-import UserController from '../controllers/user.controller';
 import authenticateToken from '../middleware/authenticateToken';
+import limiter from '../constants/routerLimiter';
 
 const router = Router();
 const playController = new PlayController()
 
-router.get('/play', authenticateToken, playController.playRound.bind(playController));
-router.get('/play/muchRounds', authenticateToken, playController.playGame.bind(playController))
+router.get('/play', authenticateToken, limiter, playController.playRound.bind(playController));
+router.get('/play/muchRounds', authenticateToken, limiter, playController.playGame.bind(playController))
 
 export default router
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
 import authenticateToken from '../middleware/authenticateToken';
+import limiter from '../constants/routerLimiter';
 
 const router = Router();
 const userController = new UserController()
 
-router.get('/user', userController.getUsers.bind(userController));
-router.post('/user/login', userController.loginUser.bind(userController));
-router.post('/user/logout', authenticateToken, userController.logoutUser.bind(userController));
-router.post('/user/register', userController.registerUser.bind(userController));
+router.get('/user', limiter, userController.getUsers.bind(userController));
+router.post('/user/login', limiter, userController.loginUser.bind(userController));
+router.post('/user/logout', authenticateToken, limiter, userController.logoutUser.bind(userController));
+router.post('/user/register', limiter, userController.registerUser.bind(userController));
 
 export default router
