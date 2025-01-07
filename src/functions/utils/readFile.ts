3acac1c6c3a96dd1d8a7738d1dac325b7f5cb55b
Luiz Felipe <felipe.pinghang2@gmail.com>
2025-01-06 21:32:49 -0300

    fix: fixing directory error in documentation index file

--- src/functions/utils/readFile.ts
+++ src/functions/utils/readFile.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from "path";
 
 function readFile(filePatch: string): any {
-    const data = fs.readFileSync(path.resolve(__dirname, `../../public/${filePatch}`), 'utf8');
+    const data = fs.readFileSync(path.resolve(__dirname, `../../../public/${filePatch}`), 'utf8');
     return data;
 }
 
